fe550b246a023bd277ab5433e226644b34159446
sacramento.marcos <sacramento.marcosf@gmail.com>
2026-03-06 14:04:39 -0300

    Implement JWT signing and decoding functions for user authentication

--- auth/auth.py
+++ auth/auth.py
@@ -9,3 +9,19 @@ def token_response(token:str):
     return {
         'access_token': token
     }
+
+def sign_jwt(user_id:str) -> dict[str:str]:
+    payload = {
+        'user_id': user_id,
+        'expires': time.time() + 600
+    }
+    token = jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
+
+    return token_response(token)
+
+def decode_jwt(token:str) -> dict:
+    try:
+        decoded_token = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
+        return decoded_token if decoded_token['expires'] >= time.time() else None
+    except:
+        return {}

bc410d8e5868a6e0064f8e5d4d54fa6fa1025523
sacramento.marcos <sacramento.marcosf@gmail.com>
2026-03-06 11:06:44 -0300

    Refactor API endpoints for emprestimos: add status codes and improve error handling

--- objects.py
+++ objects.py
@@ -2,6 +2,7 @@ from datetime import datetime, date
 from pydantic import BaseModel
 
 class Emprestimo(BaseModel):
+    # id_emprestimo: int | None = None
     id_livro: int
     id_usuario: str
     data_previsao_devolucao_em: date
--- server.py
+++ server.py
@@ -1,12 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, HTTPException
+from datetime import date
 from connection import con_engine
+from sqlalchemy import text
 import pandas as pd
 from objects import Emprestimo
 
 app = FastAPI()
 engine = con_engine()
 
-@app.get('/emprestimos/atual')
+@app.get('/emprestimos/atual', status_code=status.HTTP_200_OK)
 async def atual():
     try:
         df:pd.DataFrame = pd.read_sql_query('''select id_emprestimo
@@ -20,8 +22,8 @@ async def atual():
         print(e)
         return {f'[ERRO]':e}
 
-@app.get('/emprestimos')
-async def busca():
+@app.get('/emprestimos', status_code=status.HTTP_200_OK)
+async def busca_todos_emprestimos():
     try:
         df:pd.DataFrame = pd.read_sql_query('''select * from vw_emprestimos''', con=engine)
         tabela = df.to_dict(orient='records')
@@ -29,15 +31,41 @@ async def busca():
     except Exception as e:
         print(e)
         return {f'[ERRO]':e}
-@app.post('/emprestimos')
-async def cria(emprestimo:Emprestimo):
+@app.post('/emprestimos', status_code=status.HTTP_201_CREATED)
+async def cria_emprestimo(emprestimo:Emprestimo):
     try:
         with engine.connect() as con:
-            con.execute('''INSERT INTO ''')
+            con.execute(text(f'''INSERT INTO emprestimos
+                        (id_livro, id_usuario, data_previsao_devolucao_em, data_realizado_em)
+                        values (
+                        {emprestimo.id_livro},
+                        '{emprestimo.id_usuario}',
+                        '{emprestimo.data_previsao_devolucao_em}',
+                        '{emprestimo.data_realizado_em}'
+                        )'''))
+            con.commit()
+        return dict(emprestimo)
     except Exception as e:
         print(e)
-        return {f'[ERRO]':e}
+        # raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=e)
+        return {'[ERRO]': e}
 
-# @app.patch('/devolucoes')
+@app.patch('/devolucoes', status_code=status.HTTP_202_ACCEPTED)
+async def fecha_emprestimo(id_emprestimos:list[int]):
+    hoje = date.today()
+    try:
+        with engine.connect() as con:
+            for id in id_emprestimos:
+                con.execute(text(f'''
+                                UPDATE emprestimos
+                                SET status = FALSE,
+                                data_devolucao_em = '{hoje.isoformat()}'
+                                WHERE id_emprestimo = {id}
+                                '''))
+            con.commit()
+        return id_emprestimos
+    except Exception as e:
+        print(e)
+        return {'[ERRO]': e}
 # @app.get('/')
 
